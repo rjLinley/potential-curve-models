ac3abaf5073cd9cc7cff65a7e5ea5e0926e50f92
Richard Linley <rj.linley@gmail.com>
2025-02-02 15:26:09 -0500

    test

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+console.log('Javascript Ran 🐢')
+console.log(document.getElementById('root'))
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
